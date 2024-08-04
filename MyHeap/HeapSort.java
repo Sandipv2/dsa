7a511fe45328cf8d59f7e3f5f9a2966594e0c42e
sandipv2 <sandiipvarma@gmail.com>
2024-08-04 20:40:28 +0530

    heap change

--- MyHeap/HeapSort.java
+++ MyHeap/HeapSort.java
@@ -50,7 +50,7 @@ public class HeapSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {12, 11, 13, 5, 6, 7};
+        int[] arr = {10,20,30,40,50,60};
 
         sort(arr);
 
